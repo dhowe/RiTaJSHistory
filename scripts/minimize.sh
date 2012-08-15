2d58e2b098d60dd82f8b0243b75684b3955ba9cf
Daniel C. Howe <daniel.c.howe@gmail.com>
2012-08-15 01:13:55 -0400

    fixing
    Former-commit-id: 9e4f1b698f2887ad0e0ab5e1a93ad33247dbcca1 [formerly ab4b408d147e278bb9e69a3a1ecf77d70cb54e12]
    Former-commit-id: 03121a27ca6965c45371ea5504435c18a1c13342

--- scripts/minimize.sh
+++ scripts/minimize.sh
@@ -2,7 +2,6 @@
 
 #TODO: add flags for options
 
-<<<<<<< HEAD
 DEST=".."
 VERSION="0.XXa"
 
@@ -45,37 +44,14 @@ FULLDST=$DEST/www/download/rita_full-$VERSION.min.js
 #    --compilation_level SIMPLE_OPTIMIZATIONS
 
 ##### just rita dict      #####
-=======
-VERSION=0.14a
-LIB=../tools/compiler-latest
 
-SRCFILE=../src/rita.js
-DICTFILE=../src/rita_dict.js
-SRCFILES="${SRCFILE} --js ${DICTFILE}"
-
-DSTFILE=../www/download/rita-$VERSION.min.js
-DICTDST=../www/download/rita_dict-$VERSION.min.js
-FULLDST=../www/download/rita_full-$VERSION.min.js
-
-#filename="${fullfile##*/}"
-
-##### just the rita lib #####
-echo "compiling rita-lib to ${DSTFILE}"; java -jar $LIB/compiler.jar --js $SRCFILE  \
-    --js_output_file $DSTFILE  --summary_detail_level 3  \
-    --compilation_level SIMPLE_OPTIMIZATIONS
-
-
-##### just the rita dict #####
->>>>>>> 333adb8c05f2add16a5abb95e4aada7b2c2f4a8e
 #echo "compiling rita-dict to ${DICTDST}"; java -jar $LIB/compiler.jar --js $DICTFILE \
 #    --js_output_file $DICTDST  --summary_detail_level 3 \
 #   --compilation_level SIMPLE_OPTIMIZATIONS
 
-<<<<<<< HEAD
+
 ##### combine the 3 (core,dict,ls) #####
-=======
-##### combine the two files #####
->>>>>>> 333adb8c05f2add16a5abb95e4aada7b2c2f4a8e
+
 echo "compiling rita-full to ${FULLDST}"; java -jar $LIB/compiler.jar --js  ${SRCFILES} \
   --js_output_file $FULLDST --summary_detail_level 3 \
   --compilation_level SIMPLE_OPTIMIZATIONS
