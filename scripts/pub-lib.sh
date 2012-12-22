fb25a8d66955215c4b8a770abbe72578fd992408
Daniel C. Howe <daniel.c.howe@gmail.com>
2012-12-23 05:09:15 +0800

    updates to build/publish scripts
    Former-commit-id: 2037e569c3c200bb19c61a73493e96fff846d06d [formerly 3c2c53b130f04405cc71bb759d55981234c7c72f] [formerly 7da6cf9a8a68b344ffa7a4f5e07ffbb5c063bc48 [formerly f675b680c47bc5c7d9b9addd1e7f4062849a7154]]
    Former-commit-id: 3b746269a155434cea473615656b9cf52e4e2a8a [formerly 1d99b137136a1a58c031171d2504ad76c5529d90]
    Former-commit-id: e4ef7bf940fd4d7ce9149bc4c9dd9e621b49b672

--- scripts/pub-lib.sh
+++ scripts/pub-lib.sh
@@ -17,6 +17,13 @@ ls -l $ZIP_FILE
 echo copying $ZIP_FILE to $DEST...
 cat $ZIP_FILE | ssh $DEST "(cd /Library/WebServer/Documents/rita/js/; tar xf -; ls /Library/WebServer/Documents/rita/js)"
 
+# TODO: need to add these soft-links
+# unlink rita-latest.min.js ; ln -s rita-0.23a.min.js rita-latest.min.js
+# unlink rita-latest.js ; ln -s rita-0.23a.js rita-latest.js
+# unlink rita-latest-full.zip ; ln -s rita-full-0.23a.zip rita-latest-full.zip
+
+
+
 rm -rf $ZIP_FILE
 echo
 echo cleaning up...
