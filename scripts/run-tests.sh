5413d4eddfa49808e34dac0998236102ce4e0960
Daniel C. Howe <daniel.c.howe@gmail.com>
2013-04-03 17:33:03 +0800

    new reference generator tool
    Former-commit-id: c45f5f1c527763896395dcea62fd7cbce4502df4 [formerly 6bad196ff8523470a9730b4dd702978ee9e79e12]
    Former-commit-id: bd7ef55431610752491efda8b2a570cc9ccc135c

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -17,6 +17,6 @@ if [[ "$#" == 1 && $1 =~ ^-p$ ]]; then
 shift
 fi
 
-./run-phantom-tests.pl 2> tests-err.txt $@
+perl ./run-phantom-tests.pl 2> tests-err.txt $@
 
 echo Wrote errors to tests-err.txt
