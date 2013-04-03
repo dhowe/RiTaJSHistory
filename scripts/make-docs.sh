636e4479fd399139e4e48fcf85cd762bae8ab3dc
Daniel C. Howe <daniel.c.howe@gmail.com>
2013-04-03 17:52:05 +0800

    added refgen tool
    Former-commit-id: 97fd91e54ae79e7070c927dd2f8cd3d3bd018827 [formerly 663aee5dc14c0600d5f7e5547e1877b736258435]
    Former-commit-id: 67a76b79f1bed7e2913589b88284e73d3f6c1b71

--- scripts/make-docs.sh
+++ scripts/make-docs.sh
@@ -1,25 +1,12 @@
 #!/bin/sh
 
-if [ $# != 1 ]
-then
-  echo
-  echo "output dir required"
-  echo "usage: make-docs.sh [out-dir]"
-  exit
-fi
+INDIR=$1
+OUTDIR=$2
 
-
-#OUTDIR=$JSDOC/../../www/reference
-
-OUTDIR=$1
-SRCFILE=../src/rita.js
-JSDOC=../tools/jsdoc-toolkit
-CSS=../www/css
-
+cd ../tools/refgen # tmp: make into jar
 
 #echo copying css to $OUTDIR 
 #ls $OUTDIR
 #cp -r $CSS $OUTDIR
 
-java -Xmx512m -jar $JSDOC/jsrun.jar $JSDOC/app/run.js -d=$OUTDIR/www/reference -a \
-    -t=$JSDOC/templates/ritajs $SRCFILE > docs-err.txt
+java -Xmx512m -classpath .:core.jar:json.jar DocGenerator ../../www/reference/ ../../docs/
