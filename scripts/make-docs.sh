a95bb11a3584228e0124699e085698b9c5d299ab
Daniel C. Howe <daniel.c.howe@gmail.com>
2013-04-06 18:34:39 +0800

    Updated docs for core objects
    Former-commit-id: c85e8ee34162aa34190b9f00775037b1ec9f72fe [formerly 03b6de6e5f89502d601ec1ab74661cb94d4e8e3c]
    Former-commit-id: 06b191bfc7f0ebaf0cb4888db2708af8ddcc079d

--- scripts/make-docs.sh
+++ scripts/make-docs.sh
@@ -4,14 +4,19 @@ INPUT=../../docs/
 OUTPUT=../../www/reference/
 
 HTML="${INPUT}html"
+CSS="${INPUT}css"
 CLASSPATH=".:../../lib/core.jar:../../lib/json.jar"
 
 cd ../tools/refgen				 # tmp: use jar instead
 
-java -Xmx512m -classpath $CLASSPATH DocGenerator $OUTPUT $INPUT
-
 cp ${HTML}/*.html $OUTPUT
+cp ${CSS}/*.css ${OUTPUT}../css/
+
+
+java -Xmx512m -classpath $CLASSPATH DocGenerator $OUTPUT $INPUT
 
 rm $OUTPUT/template.html
 
+echo DONE: css written to ${OUTPUT}../css/
 echo DONE: html written to $HTML
+
