03243498d957dda405ecb1ccf89b0b6dcab17e36
Daniel C. Howe <daniel.c.howe@gmail.com>
2013-10-12 15:54:15 +0800

    v1.0.41: minor bugs fixes and node-build scripts
    Former-commit-id: f01b91b90200bc2c4b6c28c58deef3a89af679e2 [formerly ef234e38b7e4dd09ba3e5732a536f5eaacda715a]
    Former-commit-id: eeecea4b042ef2f49b47a65ad9a0565a3ed01be7

---
+++ scripts/make-node.sh
@@ -0,0 +1,94 @@
+#/bin/sh
+
+set -e # die on errors 
+
+NPM_BIN=/Users/dhowe/.nvm/v0.10.15/bin
+
+if [ $# -lt "1"  ]
+then
+    echo
+    echo "  error:   tag or version required"
+    echo
+    echo "  usage:   make-node.sh [tag]  "
+    echo "           make-node.sh 1.0.63a"
+    echo
+    echo "  options:"
+    echo "       -p = npm-publish after build "
+    exit
+fi
+
+
+VERSION=$1
+DO_PUBLISH=0
+INCLUDE_DOCS=1
+
+LINE="------------------------------------------------------"
+
+while [ $# -ge 1 ]; do
+    case $1 in
+        -D) INCLUDE_DOCS=0  ;;
+    esac
+    case $1 in
+        -p) DO_PUBLISH=1  ;;
+    esac
+    shift
+done
+
+#echo "DOCS=$INCLUDE_DOCS"
+#echo "PUB=$DO_PUBLISH"
+
+##############################################################
+
+BUILD=../build
+NODE_RES=../noderes
+NODE_DIR=$BUILD/node
+NODE_RITA=$NODE_DIR/rita
+NODE_LIB=$NODE_DIR/rita/lib
+NODE_DOC=$NODE_DIR/rita/doc
+DL_DIR=$BUILD/www/download
+DOC_DIR=$BUILD/www/reference
+PKG_JSON=$NODE_RITA/package.json
+
+echo
+echo "Building [Node] RiTaJS v$VERSION -----------------------"
+echo
+
+###COMPILE-JS###################################################
+
+echo Re-creating $NODE_LIB 
+rm -rf $NODE_DIR
+mkdir -p $NODE_LIB
+mkdir -p $NODE_DOC
+
+echo Copying $NODE_RES/package.json to $PKG_JSON
+cp $NODE_RES/package.json $PKG_JSON
+sed -i "" "s/##version##/${VERSION}/g" $PKG_JSON
+#cat $PKG_JSON
+
+echo Copying $NODE_RES/README.md to $NODE_RITA
+cp $NODE_RES/README.md $NODE_RITA/
+
+echo Copying $DOC_DIR to $NODE_DOC
+cp -r $DOC_DIR/* $NODE_DOC/
+find $NODE_DOC -name 'CVS' | xargs rm -r
+
+echo Copying $DL_DIR/rita-$VERSION.min.js to 'lib'
+cp $DL_DIR/rita-$VERSION.min.js $NODE_LIB/rita.js
+
+#ls -R $NODE_RITA
+#exit
+
+echo $LINE
+echo
+
+if [ $DO_PUBLISH = 1 ]
+then
+    echo Calling npm publish... 
+    $NPM_BIN/npm publish $NODE_RITA
+    # --force if overridding current version
+    echo Done
+else
+    echo Done [use [-p] to publish]
+fi
+
+exit
