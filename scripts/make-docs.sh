ff7bad70070a1d7fe9f284f29fbf7742269d8b8f
Daniel Howe <daniel@rednoise.org>
2014-04-02 03:52:05 +0800

    v1.0.53
    Former-commit-id: dcbe31b55030ab8477d4f60926d9de68387009d7 [formerly 10728d7a5690b85d79dfac5c00126ea2c6aa166c]
    Former-commit-id: aad971a542db06b64c862e1061288ad5456a2400

--- scripts/make-docs.sh
+++ scripts/make-docs.sh
@@ -6,8 +6,8 @@ set -e
 
 cd ../tools/refgen				 
 
-INPUT=../../docs/
-OUTPUT=../../www/reference/
+INPUT=../../../docs/
+OUTPUT=../../../www/reference/
 #GENZIP="doc-gen.zip"
 #CLASSPATH="$GENZIP:../../lib/core.jar:../../lib/json.jar"
 CLASSPATH="../../../bin:../../../libs/core.jar:../../../libs/json.jar"
