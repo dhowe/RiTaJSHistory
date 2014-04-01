654571345b34aa4a6341f78c64c74dc61f73ba4b
Daniel Howe <daniel@rednoise.org>
2014-04-02 04:03:39 +0800

    v1.0.53
    Former-commit-id: 52e033e2580dddf078d54771510457cff6ffbd3f [formerly dd584ecaacc3bae1f3bd9332c118d8af431130c4]
    Former-commit-id: fe743a35f812901916dbd5e28f98c85878607cfb

---
+++ test/renderer/data/grammarfile.js
@@ -0,0 +1,55 @@
+grammar = {
+	"<start>": 			"<noun-phrase> <verb-phrase>",
+	"<noun-phrase>": 	"<determiner> <noun>",
+	"<verb-phrase>": 	"<verb> | <verb> <noun-phrase> [.1]",
+	"<determiner>":		"a [.1] | the",
+	"<noun>":			"woman | man",
+	"<verb>":			"shoots"
+}
+
+
+/*
+# An Example CFG
+
+######################################
+# s   -> np vp
+# np  -> det n
+# vp  -> v | v np
+# det -> 'a' | 'the'
+# n   -> 'woman' | 'man'
+# v   -> 'shoots'
+######################################
+
+
+{
+  <start>
+  <noun-phrase> <verb-phrase> 
+}
+
+
+
+{
+  <noun-phrase>
+  <determiner> <noun>
+}
+
+{
+  <verb-phrase>
+  <verb> | <verb> <noun-phrase> 
+}
+
+{
+  <determiner>
+  a | the 
+}
+
+{
+  <noun>
+  woman  |  man  
+}
+
+{
+  <verb>
+  shoots
+}
+*/
