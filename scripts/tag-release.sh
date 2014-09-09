f93941338303cc0891c048e0c85946ce6fe63784
Daniel Howe <daniel@rednoise.org>
2014-09-10 01:50:51 +0800

    added git tag script
    Former-commit-id: 10388a6e5bee193716d37fd016af1e85928d8212 [formerly f52e310606c051e47c3ebb1aef8ebf0312c8f796]
    Former-commit-id: d43de6e1f8f4543211479772485871e9d5aaa6dd

---
+++ scripts/tag-release.sh
@@ -0,0 +1,23 @@
+#/bin/sh
+
+set -e # die on errors 
+
+if [ $# -lt "1"  ]
+then
+    echo
+    echo "  error:   tag or version required"
+    echo
+    echo "  usage:   tag-release.sh [tag]" 
+    exit
+fi
+
+VERSION=$1
+
+# commit your changes
+git commit -am "Release v$VERSION"
+
+# tag the commit
+git tag -a v$VERSION -m "Release v$VERSION"
+
+# push to GitHub
+git push origin master --tags  
